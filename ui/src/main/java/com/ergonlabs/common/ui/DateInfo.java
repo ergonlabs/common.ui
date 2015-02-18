7230239b921b3fa5bd71236cfc94418a86d419aa
Stefan Rusek <stefan@rusek.org>
2015-02-18 16:29:25 -0500

    updates for fancier rendering

--- ui/src/main/java/com/ergonlabs/common/ui/DateInfo.java
+++ ui/src/main/java/com/ergonlabs/common/ui/DateInfo.java
@@ -1,5 +1,9 @@
 package com.ergonlabs.common.ui;
 
+import android.view.View;
+
+import com.ergonlabs.common.ui.internal.calendar.CellView;
+
 /**
  * Created by stefanrusek on 6/11/14.
  */
@@ -8,11 +12,36 @@ public class DateInfo {
     public Integer backgroundRes;
 
     public DateInfo(long date, Integer backgroundRes) {
-        this.date = date;
+        this(date);
         this.backgroundRes = backgroundRes;
     }
 
     public DateInfo(int backgroundRes) {
+        this();
         this.backgroundRes = backgroundRes;
+    }
+
+    protected DateInfo(long date) {
+        this();
+        this.date = date;
+    }
+
+    protected DateInfo() {
+
+    }
+
+    public void setBackground(View cell) {
+        cell.setBackgroundResource(backgroundRes);
+    }
+
+    public interface Cell {
+        public void setDateInfo(DateInfo dateInfo);
+    }
+
+    public void onClick(Cell cell, long date) {
+    }
+
+    public boolean onLongClick(Cell cell, long date) {
+        return false;
     }
 }
--- ui/src/main/java/com/ergonlabs/common/ui/internal/calendar/CellView.java
+++ ui/src/main/java/com/ergonlabs/common/ui/internal/calendar/CellView.java
@@ -3,6 +3,7 @@ package com.ergonlabs.common.ui.internal.calendar;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.ergonlabs.common.ui.DateInfo;
@@ -10,7 +11,7 @@ import com.ergonlabs.common.ui.DateInfo;
 /**
  * Created by stefanrusek on 6/10/14.
  */
-public class CellView extends TextView {
+public class CellView extends TextView implements DateInfo.Cell {
 
     private long date;
 
@@ -47,8 +48,24 @@ public class CellView extends TextView {
         setBackgroundResource(android.R.color.transparent);
 
         if (dateInfo != null) {
-            if (dateInfo.backgroundRes != null)
-                setBackgroundResource(dateInfo.backgroundRes);
+            setDateInfo(dateInfo);
         }
+    }
+
+    public void setDateInfo(final DateInfo dateInfo) {
+        if (dateInfo.backgroundRes != null)
+            dateInfo.setBackground(this);
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateInfo.onClick(CellView.this, date);
+            }
+        });
+        setOnLongClickListener(new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return dateInfo.onLongClick(CellView.this, date);
+            }
+        });
     }
 }
